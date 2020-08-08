71cf738ea75c4877cc5da9bf54d1f752992ab365
Wise-Dude <67603638+Wise-Dude@users.noreply.github.com>
2020-08-08 20:30:20 +0600

    Add files via upload

--- dice_roller.py
+++ dice_roller.py
@@ -1,5 +1,19 @@
+import random
+
 def main():
-  #print('You rolled a die')
+  player_1 = random.randint(1,6)
+  player_2 = random.randint(1,6)
+
+  if player_1 == player_2:
+    print(f"It's a draw :( ")
+    print(f"Player 1 rolled : {player_1} \nPlayer 2 rolled : {player_2}")
+  elif player_1 > player_2:  
+    print("Player 1 won! ")
+    print(f"Player 1 rolled : {player_1} \nPlayer 2 rolled : {player_2}")
+  else:
+    print("player 2 won! ")
+    print(f"Player 1 rolled : {player_1} \nPlayer 2 rolled : {player_2}")
 
 if __name__== "__main__":
   main()
+  
